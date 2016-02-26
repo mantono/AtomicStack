4407346159f0ca4a3726683c5d6677e5340fbd70
mantono <anton.osterberg@gmail.com>
2016-02-26 14:41:00 +0100

    Created interface Stack.

---
+++ src/com/mantono/stack/Stack.java
@@ -0,0 +1,49 @@
+package com.mantono.stack;
+
+import java.io.Serializable;
+import java.util.EmptyStackException;
+
+/**
+ * An interface for data structures implementing stack functionality, something
+ * that is missing in the Java Utility libraries.
+ * 
+ * @author Anton Österberg
+ * 
+ * @param <T>
+ */
+public interface Stack<T> extends Serializable
+{
+	/**
+	 * Check is the is empty.
+	 * 
+	 * @return true if the stack does not contain any elements, else false.
+	 */
+	boolean empty();
+
+	/**
+	 * Looks at the element at the top of the stack, but does not remove it.
+	 * 
+	 * @return the top most element on the stack.
+	 * @throws EmptyStackException
+	 *             if the stack does not contains any elements.
+	 */
+	T peek() throws EmptyStackException;
+
+	/**
+	 * Returns and removes the element at the top of the stack.
+	 * 
+	 * @return the top most element on the stack.
+	 * @throws EmptyStackException
+	 *             if the stack does not contains any elements.
+	 */
+	T pop() throws EmptyStackException;
+
+	/**
+	 * Pushes an element onto the top of the stack.
+	 * 
+	 * @param item
+	 *            the element that will be added to the stack.
+	 * @return the same element (<code>item</code>).
+	 */
+	T push(T item);
+}
